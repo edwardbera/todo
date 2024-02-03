8c2bf70192339f4dc6aa566250c70aa3152d676f
Edward Bera <47110064+edwardbera@users.noreply.github.com>
2024-02-03 16:18:25 +0200

    - Added sqlite database. - Added a status attribute to the todos to indicate if completed or pending. - Todos can be deleted from database and their status can be updated.

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.21.0
 
-require github.com/google/uuid v1.6.0 // indirect
+require (
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-sqlite3 v1.14.21 // indirect
+)
--- main.go
+++ main.go
@@ -2,19 +2,22 @@ package main
 
 import (
 	"bufio"
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/google/uuid"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 type todo struct {
-	id          uuid.UUID
 	title       string
 	description string
 	date        time.Time
+	status      bool
 }
 
 var collection = make(map[int]todo)
@@ -24,7 +27,6 @@ func createTodo() *todo {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("What do you want to do today?")
 	t := todo{}
-	t.id = uuid.New()
 	n, err := reader.ReadString('\n')
 	t.title = n
 	if err != nil {
@@ -44,8 +46,9 @@ func createTodo() *todo {
 func inputTodo() {
 	var status string
 	for status != "n" {
-		key := len(collection) + 1
-		collection[key] = *createTodo()
+		//key := len(collection) + 1
+		//collection[key] = *createTodo()
+		insertData(createTodo())
 		fmt.Println("Do you want to add another todo? (Enter 'y' for yes or 'n' for no)")
 		_, err := fmt.Scanln(&status)
 		if err != nil {
@@ -58,29 +61,14 @@ func inputTodo() {
 
 // Receive input from user
 func getInput(option string) {
-	var response string
+
 	switch option {
 	case "1":
 		viewTodos()
-		fmt.Println("Enter 'b' to go back to menu or 'e' to exit the apllication.")
-		_, err := fmt.Scanln(&response)
-		if err != nil {
-			log.Fatal(err)
-		}
-		switch response {
-		case "b":
-			option := menu()
-			getInput(option)
-		case "e":
-			break
-		default:
-			break
-		}
+
 	case "2":
 		inputTodo()
 	case "3":
-		deleteTodo()
-	case "4":
 		break
 	default:
 		break
@@ -94,8 +82,7 @@ func menu() string {
 	fmt.Println("Menu")
 	fmt.Println("1. View Todos")
 	fmt.Println("2. Create Todo")
-	fmt.Println("3. Delete Todo")
-	fmt.Println("4. Exit")
+	fmt.Println("3. Exit")
 	_, err := fmt.Scanln(&reply)
 	if err != nil {
 		log.Fatal(err)
@@ -105,49 +92,165 @@ func menu() string {
 
 // View available tasks
 func viewTodos() {
-
-	fmt.Println("Here are your Todos")
-	fmt.Println("Index	  Title")
-	fmt.Println("-------------------------")
-	for key, value := range collection {
-		fmt.Println(key, " | ", value.title)
+	var id string
+	var response string
+	var response2 string
+	var newstatus string
+	db, err := sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		panic(err)
 	}
+
+	sts := "SELECT * FROM todos"
+
+	row, err := db.Query(sts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 	fmt.Println("-------------------------")
+	fmt.Println("Here are your Todos")
+	fmt.Println("-------------------------")
+
+	for row.Next() { // Iterate and fetch the records from result cursor
+		var title string
+		var description string
+		var date string
+		var status string
+		var completed string
+
+		row.Scan(&id, &title, &description, &date, &status)
+
+		if status == "0" {
+			completed = "pending"
+		} else {
+			completed = "completed"
+		}
+
+		fmt.Println("ID : "+id, "| Title : "+title, "Description : "+description, "Date : "+strings.Split(date, " ")[0], "Status : "+completed)
+		fmt.Println("-------------------------")
+
+	}
+
+	fmt.Println("")
+	fmt.Println("--To UPDATE a Todo enter 'U:' followed by the id of the task you wish to update.--")
+	fmt.Println("--Enter 'b' to go BACK to menu or 'e' to EXIT the appication.--")
+
+	_, err = fmt.Scanln(&response)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if strings.Contains(response, "U:") {
+		var resparr = strings.Split(response, ":")
+		fmt.Println("")
+		fmt.Println("To delete the todo input D and press Enter")
+		fmt.Println("To mark the todo as completed input C and press Enter")
+		_, err = fmt.Scanln(&response2)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if response2 == "D" {
+			deleteTodo(string(resparr[1]))
+			fmt.Println("Todo has been deleted.")
+			fmt.Println("")
+		} else if response2 == "C" {
+			newstatus = "1"
+			updateTodo(string(resparr[1]), newstatus)
+			fmt.Println("Todo status has been updated.")
+			fmt.Println("")
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		viewTodos()
+	} else {
+		switch response {
+		case "b":
+			option := menu()
+			getInput(option)
+		case "e":
+			break
+		default:
+			break
+		}
+		_, err = fmt.Scanln(&response)
+		if err != nil {
+			log.Fatal(err)
+		}
+		var resparr = strings.Split(response, ":")
+		resparr[1] = strings.TrimSpace(string(resparr[1]))
+
+	}
 
 }
 
-func deleteTodo() {
-	var key int
-	var response string
-	viewTodos()
-	fmt.Println("Enter the number for the task you want to delete")
-	_, err := fmt.Scan(&key)
+// Create SQLite Database
+func createDB() {
+	db, err := sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+	sts := `
+	CREATE TABLE IF NOT EXISTS todos(id INTEGER PRIMARY KEY, title TEXT, description TEXT, date TEXT, status TEXT);
+	`
+	_, err = db.Exec(sts)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	delete(collection, key)
+	fmt.Println("table cars created")
+}
 
-	fmt.Println("-------------------------")
-	fmt.Println("Enter 'b' to go back to menu or 'e' to exit the apllication.")
+func insertData(data *todo) {
+	db, err := sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		panic(err)
+	}
 
-	_, err2 := fmt.Scanln(&response)
-	if err2 != nil {
-		log.Fatal(err2)
+	sts := "INSERT INTO todos(title, description, date, status) VALUES ('" + data.title + "', '" + data.description + "', '" + data.date.String() + "'," + strconv.FormatBool(data.status) + " )"
+
+	_, err = db.Exec(sts)
+	if err != nil {
+		log.Fatal(err)
 	}
-	switch response {
-	case "b":
-		option := menu()
-		getInput(option)
-	case "e":
-		break
-	default:
-		break
+	defer db.Close()
+}
+
+func updateTodo(id string, status string) {
+	db, err := sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		panic(err)
 	}
+	sts := "UPDATE todos SET status=" + status + " WHERE id=" + id
+	_, err = db.Exec(sts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
+func deleteTodo(id string) {
+	db, err := sql.Open("sqlite3", "todo.db")
+	if err != nil {
+		panic(err)
+	}
+	sts := "DELETE FROM todos WHERE id=" + id
+	_, err = db.Exec(sts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
 
 func main() {
+	createDB()
 	option := menu()
 	getInput(option)
+
 }
